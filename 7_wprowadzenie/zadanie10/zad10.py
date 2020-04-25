ae3a8d9423d7a9b55600796ab8faab76d81e43ff
ki1ki1 <58849996+ki1ki1@users.noreply.github.com>
2020-04-25 15:11:00 +0200

    Update zad10.py

--- 7_wprowadzenie/zadanie10/zad10.py
+++ 7_wprowadzenie/zadanie10/zad10.py
@@ -3,4 +3,4 @@ a = np.array([[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3
 print(a)
 b = a.reshape((27,3))
 print(b)
-#Możliwośći :81x1, 1x81, 27x3, 3x27, 9x9 - tylko te możliwości zapełnią całą macierz
+#Możliwości :81x1, 1x81, 27x3, 3x27, 9x9 - tylko te możliwości zapełnią całą macierz
